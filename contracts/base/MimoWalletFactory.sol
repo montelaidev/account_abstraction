15330e07c628101114d1f838dceaaece7bc56da8
Monte Lai <monte.lai@consensys.net>
2023-04-28 16:06:45 +0800

    add factory

---
+++ contracts/base/MimoWalletFactory.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.19;
+
+import {MimoWallet} from "./MimoWallet.sol";
+
+contract MimoWalletFactory {
+    MimoWallet public immutable accountImplementation;
+
+    constructor(IEntryPoint _entryPoint) {
+        accountImplementation = new MimoWallet(_entryPoint);
+    }
+
+    /**
+     * create an account, and return its address.
+     * returns the address even if the account is already deployed.
+     * Note that during UserOperation execution, this method is called only if the account is not deployed.
+     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
+     */
+    function createAccount(
+        address owner,
+        uint256 salt
+    ) public returns (SimpleAccount ret) {
+        address addr = getAddress(owner, salt);
+        uint codeSize = addr.code.length;
+        if (codeSize > 0) {
+            return MimoWallet(payable(addr));
+        }
+        ret = SimpleAccount(
+            payable(
+                new ERC1967Proxy{salt: bytes32(salt)}(
+                    address(accountImplementation),
+                    abi.encodeCall(SimpleAccount.initialize, (owner))
+                )
+            )
+        );
+    }
+
+    /**
+     * calculate the counterfactual address of this account as it would be returned by createAccount()
+     */
+    function getAddress(
+        address owner,
+        uint256 salt
+    ) public view returns (address) {
+        return
+            Create2.computeAddress(
+                bytes32(salt),
+                keccak256(
+                    abi.encodePacked(
+                        type(ERC1967Proxy).creationCode,
+                        abi.encode(
+                            address(accountImplementation),
+                            abi.encodeCall(SimpleAccount.initialize, (owner))
+                        )
+                    )
+                )
+            );
+    }
+}

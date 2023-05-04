d70ab69ce4be1784ad42b1f713ed846a4c31ebe1
Monte Lai <monte.lai@consensys.net>
2023-05-04 20:22:43 +0800

    factory test

--- test/mimoAccount.test.ts
+++ test/mimoAccount.test.ts
@@ -72,4 +72,49 @@ describe("Token contract", function () {
     };
   }
 
+  describe("MimoWalletFactory", () => {
+    it("should deploy to a deterministic address", async () => {
+      const { factory, accountOwner } = await deployFixtures();
+
+      const salt = 0;
+      const address = await factory.getAddress(accountOwner.address, salt);
+
+      console.log(
+        `Deploying to ${address}, owner is ${accountOwner.address}, salt is ${salt}`
+      );
+
+      // contract should not have been deployed
+      expect(
+        await ethers.provider.getCode(address).then((code) => code.length)
+      ).to.equal("0x".length);
+
+      await factory.createAccount(accountOwner.address, salt, []);
+
+      expect(
+        await ethers.provider.getCode(address).then((code) => code.length)
+      ).to.gt("0x".length);
+    });
+
+    it("should deploy to a different address based on a different salt", async () => {
+      const { factory, accountOwner } = await deployFixtures();
+
+      const salt1 = 0;
+      const address1 = await factory.getAddress(accountOwner.address, salt1);
+
+      const salt2 = 1;
+      const address2 = await factory.getAddress(accountOwner.address, salt2);
+
+      expect(address1).to.not.equal(address2);
+
+      await factory.createAccount(accountOwner.address, salt1, []);
+      await factory.createAccount(accountOwner.address, salt2, []);
+
+      expect(
+        await ethers.provider.getCode(address1).then((code) => code.length)
+      ).to.gt("0x".length);
+      expect(
+        await ethers.provider.getCode(address2).then((code) => code.length)
+      ).to.gt("0x".length);
+    });
+  });
 });

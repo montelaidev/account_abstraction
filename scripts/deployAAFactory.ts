ca19cdac22561b9ceae82e9c23be4a59296dbd8f
Monte Lai <monte.lai@consensys.net>
2023-05-09 00:07:32 +0800

    aaFactoryScript

---
+++ scripts/deployAAFactory.ts
@@ -0,0 +1,30 @@
+import { ethers } from "hardhat";
+import deployments from "../deployments.json";
+import fs from "fs";
+
+async function main() {
+  const MimoWalletFactory = await ethers.getContractFactory(
+    "MimoWalletFactory"
+  );
+  const mimoWalletFactory = await MimoWalletFactory.deploy(
+    deployments.entryPoint
+  );
+  await mimoWalletFactory.deployed();
+
+  console.log(`MimoWalletFactory deployed to ${mimoWalletFactory.address}`);
+  fs.writeFileSync(
+    "../deployments.json",
+    JSON.stringify({
+      ...deployments,
+      aaFactory: mimoWalletFactory.address,
+    })
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  a;
+  process.exitCode = 1;
+});

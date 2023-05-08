8dd52a4edbaf02d97d40da4dcb7370426e4c532a
Monte Lai <monte.lai@consensys.net>
2023-05-08 17:22:13 +0800

    add calldataKeccak from AA ref

--- contracts/aa-4337/core/Helpers.sol
+++ contracts/aa-4337/core/Helpers.sol
@@ -84,3 +84,16 @@ function _packValidationData(
         (uint256(validUntil) << 160) |
         (uint256(validAfter) << (160 + 48));
 }
+
+/**
+ * keccak function over calldata.
+ * @dev copy calldata into memory, do keccak and drop allocated memory. Strangely, this is more efficient than letting solidity do it.
+ */
+function calldataKeccak(bytes calldata data) pure returns (bytes32 ret) {
+    assembly {
+        let mem := mload(0x40)
+        let len := data.length
+        calldatacopy(mem, data.offset, len)
+        ret := keccak256(mem, len)
+    }
+}
